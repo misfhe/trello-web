003446c30ac5d27885fc0f8ba5c7ccf5eb441bdc
minh.nguyenquang3 <minh.nguyenquang3@fssc.com.vn>
2023-12-02 20:41:38 +0700

    customize Globalcss

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from './theme'
+import { GlobalStyles } from '@mui/material'
+
+const inputGlobalStyles = <GlobalStyles styles={{ div:{ color: 'grey' } }} />
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <CssVarsProvider theme={theme}>
       <CssBaseline />
+      {/* test GlobalStyles doc: https://mui.com/material-ui/customization/how-to-customize/*/}
+      {/* <GlobalStyles styles={{ div:{ color: 'grey' } }} /> */}
+      {inputGlobalStyles}
       <App />
     </CssVarsProvider>
   </>
--- src/theme.js
+++ src/theme.js
@@ -9,12 +9,27 @@ const theme = extendTheme({
         primary: teal,
         secondary: deepOrange
       }
+      // },
+      // test components MuiCSSBaseline
+      // components:{
+      //   MuiCssBaseline: {
+      //     styleOverrides: `
+      //       div {
+      //         color: grey;
+      //       }
+      //     `
+      //   }
+      // }
+
+      // test spacing
+      // spacing: (factor) => `${0.25 * factor}rem`
     },
     dark: {
       palette: {
         primary: cyan,
         secondary: orange
       }
+      // spacing: (factor) => `${0.25 * factor}rem`
     }
   }
   // ...other properties
