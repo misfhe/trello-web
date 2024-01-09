87c610c5c16371b13300e145364e79d4ae1b21e7
minh.nguyenquang3 <minh.nguyenquang3@fssc.com.vn>
2024-01-09 20:58:20 +0700

    order array based on other array use on columns and cards

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,7 +1,9 @@
 import { Box } from '@mui/material'
 import ListColumns from './ListColumns/ListColumns'
+import { mapOrder } from '~/utils/sort'
 
 function BoardContent({ board }) {
+  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
   return (
     <Box sx={{
       bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
@@ -9,7 +11,7 @@ function BoardContent({ board }) {
       height: (theme) => theme.trello.boardContentHeight,
       p: '10px 0'
     }}>
-      <ListColumns columns = {board?.columns}/>
+      <ListColumns columns = { orderedColumns }/>
     </Box>
   )
 }
